3a7f5fdba2719231d62f1f85966263db1aef8337
Jake Hamilton <jake.hamilton@scrollmotion.com>
2017-03-25 18:45:43 -0700

    [0.0.5] Recompile module and version bump

--- dist/definitions/middleware.js
+++ dist/definitions/middleware.js
@@ -1,1 +1,15 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Middleware = function Middleware() {
+    _classCallCheck(this, Middleware);
+
+    this.name = 'unnamed-middleware';
+};
+
+exports.default = Middleware;
