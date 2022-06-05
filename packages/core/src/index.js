f0ca0022fe2f70c13cb9a2ed3a4fb17947c70fca
Jake Hamilton <jake.hamilton@hey.com>
2022-06-05 11:30:40 -0700

    fix: add missing export for hooks data symbol

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,5 +1,6 @@
 const { default: manager, Manager } = require("./manager");
 const { hooks } = require("./hooks/hooksSystem");
+const { HOOKS_DATA } = require("./util/symbols");
 
 module.exports = {
     Manager,
@@ -11,5 +12,6 @@ module.exports = {
     once: manager.once,
     emit: manager.emit,
     emitter: manager.emitter,
+    HOOKS_DATA,
     ...hooks,
 };
